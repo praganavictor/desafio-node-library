825fa97fb8786f619cf25f98f9c95741d30514b3
Victor Pragana <victormerigue@gmail.com>
2021-04-03 08:41:00 -0300

    controller user

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
--- routes.js
+++ routes.js
@@ -1,3 +1,8 @@
 const routes = require("express").Router();
 
+const userController = require("./controllers/userController");
+
+routes.post("/register", userController.register);
+routes.post("/authenticate", userController.authenticate);
+
 module.exports = routes;
